3b5769a8fe5462357bb37bfbf42dda0036644ed1
Biswajit Mohanty <biswazr@gmail.com>
2023-02-22 17:48:49 +0530

    uri in place of host

--- mongodb_consistent_backup/Backup/Mongodump/MongodumpThread.py
+++ mongodb_consistent_backup/Backup/Mongodump/MongodumpThread.py
@@ -192,7 +192,7 @@ class MongodumpThread(Process):
         ## TODO we can support compress with srv too ,but as of 22feb we donot require the feature can be a point for improvement
         elif self.is_version_gte("4.2.0"):
             auth_details=self.auth_db_insert(mongodump_flags)
-            parsed_uri = "mongodb://%s:%s@%s/?authSource=%s&compressors=snappy&readPreference=secondary" % (urllib.quote_plus(auth_details['user']), urllib.quote_plus(auth_details['password']), mongodump_uri.host,auth_details['authdb'])
+            parsed_uri = 'mongodb://%s:%s@%s/?authSource=%s&compressors=snappy&readPreference=secondary' % (urllib.quote_plus(auth_details['user']), urllib.quote_plus(auth_details['password']), mongodump_uri.host,auth_details['authdb'])
             mongodump_flags.extend([
             "--host=%s" % parsed_uri,
             ])
