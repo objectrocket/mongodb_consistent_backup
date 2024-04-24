6e66dbd54160c0324a7887fade8225ea67829631
Swapnika-21 <swapnika@objectrocket.com>
2024-04-24 16:27:54 +0530

    Try

--- mongodb_consistent_backup/Common/LocalCommand.py
+++ mongodb_consistent_backup/Common/LocalCommand.py
@@ -43,8 +43,10 @@ class LocalCommand:
 
     def run(self):
         try:
-            print(" ".join(["export GZIP=-1"]+ ["&&"] +self.admin_command_line+ ["&&"]+ self.command_line))
-            cmd = " ".join(["export GZIP=-1"]+ ["&&"] +self.admin_command_line+ ["&&"]+ self.command_line)
+            # print(" ".join(["export GZIP=-1"]+ ["&&"] +self.admin_command_line+ ["&&"]+ self.command_line))
+            # cmd = " ".join(["export GZIP=-1"]+ ["&&"] +self.admin_command_line+ ["&&"]+ self.command_line)
+            print(" ".join(self.admin_command_line+ ["&&"]+ self.command_line))
+            cmd = " ".join(self.admin_command_line+ ["&&"]+ self.command_line)            
             self._process = Popen(cmd, stdout=PIPE, stderr=PIPE,shell= True)
             while self._process.poll() is None:
                 self.parse_output()
